0730fbc205199c0abb3e0b01dc6ebaec7d60088d
Rafe Colburn <rafeco@gmail.com>
2012-03-11 19:10:42 -0400

    all set up for 2012

--- picker.rb
+++ picker.rb
@@ -16,6 +16,9 @@ def reduce(bracket)
     opponent_a = bracket[index]
     opponent_b = bracket[bracket.length - index - 1]
 
+    puts "Can't find team #{opponent_a}" if (!$teams.include?(opponent_a)) 
+    puts "Can't find team #{opponent_b}" if (!$teams.include?(opponent_b)) 
+
     winning_pct_for_a = log5($teams[opponent_a], $teams[opponent_b])
 
     puts opponent_a + " has a " + (winning_pct_for_a * 100).round().to_s + "% chance of beating " + opponent_b
@@ -32,6 +35,11 @@ def reduce(bracket)
   return winners
 end
 
+def play_in_game(game, teams) 
+  puts "Play-in #{game}"
+  reduce(teams)
+end
+
 # Read in the ratings from Ken Pomeroy's Web site. A data entry avoidance technique,
 # highly brittle. The result is a hash with the names of the schools as keys and
 # a percentage chance of winning as the values. Percentages are versus an average
@@ -44,31 +52,37 @@ File.open("ratings.tsv").each do |line|
 end
 
 # Must be in order of seeding. Names must match the keys in $teams
-west = [ "Syracuse", "Kansas St.", "Pittsburgh", "Vanderbilt", "Butler", "Xavier", "Brigham Young", 
-  "Gonzaga", "Florida St.", "Florida", "Minnesota", "Texas El Paso", "Murray St.", "Oakland", 
-  "North Texas", "Vermont" ]
+#
+# Pick play-in game winners.
+round_1_g1 = play_in_game("Game 1", ["Mississippi Valley St.", "Western Kentucky"])
+round_1_g2 = play_in_game("Game 2", ["Brigham Young", "Iona"])
+round_1_g3 = play_in_game("Game 3", ["Lamar", "Vermont"])
+round_1_g4 = play_in_game("Game 4", ["California", "South Florida"])
 
-east = [ "Kentucky", "West Virginia", "New Mexico", "Wisconsin", "Temple", "Marquette", "Clemson",
-  "Texas", "Wake Forest", "Missouri", "Washington", "Cornell", "Wofford", "Montana", "East Tennessee St.",
-  "Morgan St." ]
+west = [ "Michigan St.", "Missouri", "Marquette", "Louisville", "New Mexico", "Murray St.", "Florida", 
+  "Memphis", "St. Louis", "Virginia", "Colorado St.", "Long Beach St.", "Davidson", round_1_g2[0],
+  "Norfolk St.", "Long Island" ]
 
-midwest = [ "Kansas", "Ohio St.", "Georgetown", "Maryland", "Michigan St.", "Tennessee", "Oklahoma St.", 
-  "Nevada Las Vegas", "Northern Iowa", "Georgia Tech", "San Diego St.", "New Mexico St.", "Houston", "Ohio", 
-  "UC Santa Barbara",  "Lehigh" ]
+east = [ "Syracuse", "Ohio St.", "Florida St.", "Wisconsin", "Vanderbilt", "Cincinnati", "Gonzaga",
+  "Kansas St.", "Southern Mississippi", "West Virginia", "Texas", "Harvard", "Montana", "St. Bonaventure", 
+  "Loyola MD", "NC Asheville"]
 
-# Pick a winner of the play-in game.
-play_in_winners = reduce(["Winthrop", "Arkansas Pine Bluff"])
+midwest = [ "North Carolina", "Kansas", "Georgetown", "Michigan", "Temple", "San Diego St.", "St. Mary's", 
+  "Creighton", "Alabama", "Purdue", "North Carolina St.", round_1_g4[0], "Ohio", "Belmont", 
+  "Detroit",  round_1_g3[0] ]
 
-south = [ "Duke", "Villanova", "Baylor", "Purdue", "Texas A&M", "Notre Dame", "Richmond", "California", 
-  "Louisville", "St. Mary's", "Old Dominion", "Utah St.", "Siena", "Sam Houston St.", "Robert Morris", play_in_winners[0] ]  
+south = [ "Kentucky", "Duke", "Baylor", "Indiana", "Wichita St.", "Nevada Las Vegas", "Notre Dame", "Iowa St.", 
+  "Connecticut", "Xavier", "Colorado", "Virginia Commonwealth", "New Mexico St.", "South Dakota St.", "Lehigh", round_1_g1[0] ]  
 
 # This order is important so that the proper teams meet in the final four.
-regions = [ midwest, east, south, west ]
+regions = { "Midwest" => midwest, "East" => east, "South" => south, "West" => west }
 
 final_four_teams = []
 
 # Play through all four regionals and come up with a Final Four
-regions.each do |region|
+regions.each do |name, region|
+  puts "*** REGION: #{name} ***"
+
   rounds = 1
 
   while region.length() > 1
@@ -76,12 +90,13 @@ regions.each do |region|
     region = reduce(region)
     rounds = rounds + 1
   end
-  
+
   final_four_teams << region[0]
 end
 
-puts "FINAL FOUR"
+puts "---===> FINAL FOUR <===---"
 finalists = reduce(final_four_teams)
 
-puts "NATIONAL CHAMPION" 
+puts "============ NATIONAL CHAMPION ============"
 reduce(finalists)
+
